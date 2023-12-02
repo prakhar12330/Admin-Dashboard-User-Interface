dbda05ec788b5de242baae6f35aca2849c248410
Prakhar Singh <prakhar12330@gmail.com>
2023-12-02 14:32:20 +0530

    First Commit

---
+++ src/components/DeleteSelectedButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DeleteSelectedButton = ({ handleDeleteSelected, selectedRows }) => {
+  return (
+    <button
+      className="delete-selected-button"
+      onClick={handleDeleteSelected}
+      disabled={selectedRows.length === 0}
+    >
+      Delete Selected
+    </button>
+  );
+};
+
+export default DeleteSelectedButton;
+
+
